78d5bca31d3f9d3aba1eb6a053f076aea2be2445
Pah <pah.dsf@gmail.com>
2020-09-26 10:55:21 -0300

    setting alias for base URL

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          types: './src/types',
+          navigation: './src/navigation',
+          helpers: './src/helpers',
+          components: './src/components',
+          views: './src/views',
+        },
+      },
+    ],
+  ],
+}
